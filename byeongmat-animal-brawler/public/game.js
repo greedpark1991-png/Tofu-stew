a250095941a761378c5a989d9dee89b3116037c9
greedpark1991-png <greedpark1991@gmail.com>
2026-09-07 23:54:29 +0900

    Update game.js

--- byeongmat-animal-brawler/public/game.js
+++ byeongmat-animal-brawler/public/game.js
@@ -1,4 +1,4 @@
-import * as THREE from '/vendor/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.185.0/build/three.module.js';
 
 const socket = io();
 const $ = s => document.querySelector(s);
